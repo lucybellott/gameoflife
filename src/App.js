599be62706e621a4813154a2c1d713fdd8b2238d
Lucy <lucybellott@gmail.com>
2022-08-23 13:31:06 -0400

    grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+
 
 function App() {
+
+const rows = 40
+const cols = 40
+
+const gridOfNums = () => {
+  const gridArray = []
+  let i;
+  let j;
+  for(i=0; i<rows; i++) {
+    const row = []
+
+    for(j=0; j<cols; j++) {
+      row.push(Math.floor(Math.random()*2))
+    }
+
+      gridArray.push(row)
+  
+  }
+
+    return gridArray
+}
+
+
+const [grid, setGrid] = useState(gridOfNums)
+
+
+console.log(grid)
+
+
+
+
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1>hi there</h1>
     </div>
   );
 }
